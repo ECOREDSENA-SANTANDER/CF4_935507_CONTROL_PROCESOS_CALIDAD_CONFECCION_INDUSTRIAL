21fbf9aa7132111f60508269853dd7ce9047010f
PABG1990 <pedro19908454@gmail.com>
2026-06-26 11:27:00 -0500

    Act

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -534,7 +534,7 @@
       .col-lg-12
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2DkN1Ur5Dw0?si=ST0suG9DD1lubQeE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/OdSaHh6PQqY?si=xmxWBkVmTMg-9rU1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
         figcaption Video. #[em Panty] tanga
     
     .tit.mb-5(data-aos="fade-right")
